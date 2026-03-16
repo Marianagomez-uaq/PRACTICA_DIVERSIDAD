473d2102ea6a62d1379a59f5fd8ede3e97d27162
Mariana Gómez Becerra <mgomez110@alumnos.uaq.mx>
2026-03-16 15:05:01 -0600

    me falto poner esta de las gráficas en el commit anterior

--- R/Graficas.R
+++ R/Graficas.R
@@ -6,4 +6,14 @@
 
 # Rango-abundancia
 
+abu <- c (4, 7, 10, 1)
+rank_abundance <- function (abu) {
+  
+  ordenado <- sort (abu, decreasing = T)
+  plot (ordenado)
+}
+
+rank_abundance(abu)
+
+
 # Rarefacción
