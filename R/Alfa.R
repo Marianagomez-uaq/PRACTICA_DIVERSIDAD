d8cfcf3da563b5f05fab4c7f49626b29207e9c16
Mariana Gómez Becerra <mgomez110@alumnos.uaq.mx>
2026-03-16 15:04:37 -0600

    Cargué las bases de datos y puse la función preeliminar de shannon y rank-abundance (basado en mis scripts de tareas)

--- R/Alfa.R
+++ R/Alfa.R
@@ -3,10 +3,42 @@
 
 ################################################################################
 
+Abundancias <- read.csv ("data/Abundancias.csv")
+
+row.names (Abundancias) <- Abundancias$Muestra
+Abundancias <- Abundancias [,2:13]
+
+Rarefaccion <- read.csv ("data/Rarefaccion.csv")
+
+row.names (Rarefaccion) <- Rarefaccion$Muestra
+Rarefaccion <- Rarefaccion [,2:6]
+
+
 
 # Riqueza
 
 # Shannon
+abund2 <- c (50, 50, 50)
+
+Shannon <- function (abund) {
+
+  riqueza <- length (abund)
+  total <- sum (abund)
+  frec <- abund/total
+  mult <- (log(frec))*frec
+  s <- -(sum(mult))
+  print (s)
+  p <- s/log(riqueza)
+  
+  message ("Índice de Shannon")
+  print (s)
+  
+  message ("Pielou")
+  print (p)
+}
+
+Shannon (abund2)
+
 
 # Simpson
 
