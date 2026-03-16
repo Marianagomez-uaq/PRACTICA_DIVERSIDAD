78f8133863b356f5781e025f3e9f036f0acfd1e7
Mariana Gómez Becerra <mgomez110@alumnos.uaq.mx>
2026-03-16 16:05:55 -0600

    Agregué una base de datos que en lugar de tener 0 tiene NA, para poder sacar las riquezas. Hice una función para calcular la riqueza de cualquier archivo de abundancias.

--- R/Alfa.R
+++ R/Alfa.R
@@ -3,7 +3,7 @@
 
 ################################################################################
 
-Abundancias <- read.csv ("data/Abundancias.csv")
+Abundancias_con_ceros <- read.csv ("data/Abundancias.csv")
 
 row.names (Abundancias) <- Abundancias$Muestra
 Abundancias <- Abundancias [,2:13]
@@ -13,9 +13,38 @@ Rarefaccion <- read.csv ("data/Rarefaccion.csv")
 row.names (Rarefaccion) <- Rarefaccion$Muestra
 Rarefaccion <- Rarefaccion [,2:6]
 
+Abundanciasexperimento <- read.csv ("data/Abundanciasexperimento.csv")
+
+row.names (Abundanciasexperimento) <- Abundanciasexperimento$Muestra
+Abundanciasexperimento <- Abundanciasexperimento [,2:13]
+
+Abundanciasexperimento
+
 
 
 # Riqueza
+
+Riqueza <- function (abundancias) {
+  
+  riquezas <- data.frame (Riqueza = numeric()) # Crea un data
+  
+  for (i in 1:nrow(abundancias)) {
+    vector <- abundancias[i,]
+    vector <- as.numeric (vector)
+    vector_sin_NA <- na.omit(vector)
+    # colnames(vector) <- NULL
+    riquezas[i, 1] <- length(vector_sin_NA)
+  }
+  
+  row.names(riquezas) <- row.names (Abundancias)
+  
+  return(riquezas)
+
+}
+
+Riqueza (Abundanciasexperimento)
+
+
 
 # Shannon
 abund2 <- c (50, 50, 50)
