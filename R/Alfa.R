91040db4ba8ac988b55d3e2bcac538e1f08b9ae7
Mariana Gómez Becerra <mgomez110@alumnos.uaq.mx>
2026-03-16 19:43:39 -0600

    Correcciones en las funciones anteriores, y función para Chao1

--- R/Alfa.R
+++ R/Alfa.R
@@ -4,29 +4,20 @@
 ################################################################################
 
 Abundancias <- read.csv ("data/Abundancias.csv")
-
 row.names (Abundancias) <- Abundancias$Muestra
 Abundancias <- Abundancias [,2:13]
 
 Rarefaccion <- read.csv ("data/Rarefaccion.csv")
-
 row.names (Rarefaccion) <- Rarefaccion$Muestra
 Rarefaccion <- Rarefaccion [,2:6]
 
 Abundanciasexperimento <- read.csv ("data/Abundanciasexperimento.csv")
-
 row.names (Abundanciasexperimento) <- Abundanciasexperimento$Muestra
 Abundanciasexperimento <- Abundanciasexperimento [,2:13]
 
-Abundanciasexperimento
 
-install.packages("vegan")
-library(vegan)
 
 # Riqueza
-
-riq <- specnumber(Abundancias)
-riq
 
 Riqueza <- function (abundancias) {
   
@@ -42,7 +33,7 @@ Riqueza <- function (abundancias) {
     
   }
   
-  row.names(riquezas) <- row.names (Abundancias) # Le pone el nombre de los sitios a su respectiva riqueza
+  row.names(riquezas) <- row.names (abundancias) # Le pone el nombre de los sitios a su respectiva riqueza
   
   return(riquezas)
 
@@ -52,12 +43,12 @@ Riqueza (Abundanciasexperimento)
 
 
 
-# Shannon
+# Shannon y Pielou
 
 Shannon <- function (abundancias) { # Modificada de mi propio script (Gómez Becerra, 2026).
 
   shan <- data.frame (Shannon = numeric(), Pielou = numeric ()) # dataframe con 2 columnas vacías
-  riqueza <- Riqueza (Abundanciasexperimento) # Riqueza usando la función creada anteriormente
+  riqueza <- Riqueza (abundancias) # Riqueza usando la función creada anteriormente
   
   for (i in 1:nrow(abundancias)) { # Un ciclo por cada sitio
     
@@ -74,7 +65,7 @@ Shannon <- function (abundancias) { # Modificada de mi propio script (Gómez Bec
     shan [i, 2] <- p
     }
   }
-  row.names(shan) <- row.names (Abundancias)
+  row.names(shan) <- row.names (abundancias)
   return(shan)
 }
 
@@ -85,8 +76,33 @@ Shannon (Abundanciasexperimento)
 
 # Simpson inverso
 
-# Pielou
-
 # Chao1
+
+Chao1 <- function (abundancias) {
+  
+  riqueza <- Riqueza (abundancias)
+  chao <- data.frame (Chao1 = numeric ())
+  
+  for (i in 1:nrow(abundancias)) {
+    
+    vector <- abundancias[i,] # Toma las abundancias del sitio i
+    vector <- as.numeric (vector) # Convierte el objeto a vector, porque era un dataframe y con dataframe no funciona na.omit
+    
+    singletons <- sum(vector == 1, na.rm = T)
+    doubletons <- sum(vector == 2, na.rm = T)
+  
+    if (singletons == 0 & doubletons == 0) { # Si no hay singletons ni doubletons, se dividiría 0/0, dando como resultado NaN. Por eso pongo un if, para que ponga texto en lugar de NaN.
+      chao [i, 1] <- "No se puede calcular"
+    } else if (doubletons == 0) { # Si sí hay singletons pero no doubletons, se dividiría x/0, lo que da Inf. Por eso pongo un if, para que ponga texto en lugar de Inf.
+      chao [i, 1] <- "No se puede calcular"
+    } else { 
+      chao [i, 1] <- (riqueza[i, 1])+((singletons^2)/(2*doubletons))
+    }
+  }
+  row.names(chao) <- row.names (abundancias)
+  return(chao)
+}
+
+Chao1 (Abundanciasexperimento)
 
 # Tabla resumen
