7518960a26115950d9b3467c2559355b4001486c
Mariana Gómez Becerra <mgomez110@alumnos.uaq.mx>
2026-03-17 23:06:26 -0600

    Función rank abundance, archivos de figuras

--- R/Graficas.R
+++ R/Graficas.R
@@ -3,17 +3,36 @@
 
 ################################################################################
 
+library (ggplot2)
 
 # Rango-abundancia
 
-abu <- c (4, 7, 10, 1)
-rank_abundance <- function (abu) {
+rank_abundance <- function (abundancias) {
   
-  ordenado <- sort (abu, decreasing = T)
-  plot (ordenado)
+  for (i in 1:nrow(abundancias)) { # un ciclo por cada sitio
+    
+    trans <- t(abundancias) # Inicialmente lo hice así porque por alguna razón se pierden los nombres sin la transpuesta, pero después me dí cuenta que no era necesario que tuviera los nombres, pero como el código funciona lo dejé así.
+    
+    vector <- trans[,i] # Sitio i
+    vector <- vector[!is.na(vector)] # Elimina las especies no presentes
+    ordenado <- sort (vector, decreasing = T) # Ordena por abundancia de mayor a menor
+    ordenado <- as.data.frame(ordenado) # convierte a dataframe porque ggplot no acepta vectores
+    
+    nombre_archivo <- paste0 ("figures/rank_abundance_", i, ".png") # Con esto se genera un nombre único para cada sitio
+    titulo <- paste0 ("Rango-Abundancia en Sitio", i)
+    
+    png (nombre_archivo) # El archivo será png, contiene todo lo que esté antes de dev.off()
+    print (ggplot (ordenado, aes (x = which(ordenado > 0), y = log10(ordenado))) + # which indica la posición de los elementos que cumplen la condición (en este caso todos porque eliminé las NA), por lo que el eje de las X es el orden de cada especie. Y es el log10 las abundancias
+             geom_line () + # Genera una linea
+             geom_point() + # genera los puntos
+             labs (title = titulo, # le pone el título que hice anteriormente
+                   x = "Orden (Rango)",
+                   y = "log10 (Abundancia)"))
+    dev.off()
+  }
 }
 
-rank_abundance(abu)
+rank_abundance (Abundanciasexperimento)
 
 
 # Rarefacción
